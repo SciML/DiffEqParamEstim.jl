0f29b160aa4c67ade647cf28e56f40a535cafbf0
ChrisRackauckas <me@chrisrackauckas.com>
2016-10-29 13:14:01 -0700

    working esitmation example

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Base.Test
 f = @ode_def_nohes LotkaVolterraTest begin
   dx = a*x - b*x*y
   dy = -c*y + d*x*y
-end a=>1.5 b=>1.0 c=>3.0 d=1.0
+end a=>1.5 b=1.0 c=3.0 d=1.0
 
 u0 = [1.0;1.0]
 tspan = [0;10.0]
@@ -17,7 +17,7 @@ gr()
 plot(sol)
 
 t = collect(linspace(0,10,200))
-randomized = [(sol(t[i]) + .5randn(2)) for i in 1:length(t)]
+randomized = [(sol(t[i]) + .01randn(2)) for i in 1:length(t)]
 
 plotseries = Matrix{Float64}(length(randomized),length(u0))
 for i in 1:length(randomized)
@@ -27,7 +27,7 @@ scatter!(t,plotseries)
 
 scatter(t,plotseries)
 
-f = LotkaVolterraTest(a=1.3,b=1.0,c=2.7)
+f = LotkaVolterraTest(a=1.42)
 prob = ODEProblem(f,u0)
 sol = solve(prob,tspan)
 plot!(sol)
@@ -45,7 +45,7 @@ function model(t,p)
   vec(y)
 end
 
-fit = curve_fit(model,t,vec(plotseries),[1.0,1.0,2.0])
+fit = curve_fit(model,t,vec(plotseries),[1.42],show_trace=true,lambda=10000.0)
 
 #param = [3.0;5.0;5.0]
 param = fit.param
@@ -53,6 +53,6 @@ for i in eachindex(f.params)
   setfield!(f,f.params[i],param[i])
 end
 #prob = ODEProblem(f,u0)
-sol = solve(prob,tspan)
-plot(sol)
+sol_optimized = solve(prob,tspan)
+plot(sol_optimized)
 scatter!(t,plotseries)
