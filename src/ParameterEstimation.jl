b70a88c4ec6d9112559d34d85ae5cce78bb2e80a
ChrisRackauckas <me@chrisrackauckas.com>
2016-10-30 01:23:20 -0700

    working methods

--- src/ParameterEstimation.jl
+++ src/ParameterEstimation.jl
@@ -1,5 +1,42 @@
 module ParameterEstimation
 
-# package code goes here
+using DiffEqBase, LsqFit, OrdinaryDiffEq, LossFunctions
+
+
+  ### LsqFit Method
+  function lm_fit(prob::DEProblem,tspan,t,data,p0;kwargs...)
+    f = prob.f
+
+    model = function (t,p)
+      for i in eachindex(f.params)
+        setfield!(f,f.params[i],p[i])
+      end
+      sol = solve(prob,tspan)
+      vecout = sol(t)
+      y = Matrix{Float64}(length(t),length(prob.u₀))
+      for i in 1:length(t)
+        y[i,:] = vecout[i]
+      end
+      vec(y)
+    end
+    curve_fit(model,t,vec(data),p0;kwargs...)
+  end
+
+  function build_optim_objective(prob::DEProblem,tspan,t,data;loss_func = L2DistLoss)
+    f = prob.f
+    cost_function = function (p)
+      for i in eachindex(f.params)
+        setfield!(f,f.params[i],p[i])
+      end
+      sol = solve(prob,tspan)
+      vecout = sol(t)
+      y = Matrix{Float64}(length(t),length(prob.u₀))
+      for i in 1:length(t)
+        y[i,:] = vecout[i]
+      end
+      norm(value(loss_func(),vec(y),vec(data)))
+    end
+  end
+export lm_fit, build_optim_objective
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using ParameterEstimation, OrdinaryDiffEq, ParameterizedFunctions, LsqFit
+using ParameterEstimation, OrdinaryDiffEq, ParameterizedFunctions
 using Base.Test
 
 f = @ode_def_nohes LotkaVolterraTest begin
@@ -12,48 +12,68 @@ tspan = [0;10.0]
 prob = ODEProblem(f,u0)
 sol = solve(prob,tspan)
 
-using Plots
-gr()
-plot(sol)
+#using Plots
+#gr()
+#plot(sol)
 
 t = collect(linspace(0,10,200))
 randomized = [(sol(t[i]) + .01randn(2)) for i in 1:length(t)]
 
-plotseries = Matrix{Float64}(length(randomized),length(u0))
+data = Matrix{Float64}(length(randomized),length(u0))
 for i in 1:length(randomized)
-  plotseries[i,:] = randomized[i]
+  data[i,:] = randomized[i]
 end
-scatter!(t,plotseries)
-
-scatter(t,plotseries)
+#scatter!(t,data)
+#scatter(t,data)
 
 f = LotkaVolterraTest(a=1.42)
 prob = ODEProblem(f,u0)
 sol = solve(prob,tspan)
-plot!(sol)
+#plot!(sol)
 
-function model(t,p)
+fit = lm_fit(prob,tspan,t,vec(data),[1.49],show_trace=true,lambda=10000.0)
+param = fit.param
+# @test param[1] ≈ 1.5 # Fails because Optim's fails...
+#for i in eachindex(f.params)
+#  setfield!(f,f.params[i],param[i])
+#end
+#prob = ODEProblem(f,u0)
+#sol_optimized = solve(prob,tspan)
+#plot(sol_optimized,leg=false)
+#scatter!(t,data)
+#plot!(sol)
+
+### Optim Method
+using Optim
+cost_function = build_optim_objective(prob,tspan,t,data)
+
+result = optimize(cost_function, 0.0, 10.0)
+@test 1.5 - result.minimum[1] < 0.01
+sol_optimized = solve(prob,tspan)
+#plot(sol_optimized,leg=false)
+#scatter!(t,data)
+#plot!(sol)
+
+result = optimize(cost_function, [1.42], BFGS())
+@test 1.5 - result.minimum[1] < 0.01
+#sol_optimized2 = solve(prob,tspan)
+#plot!(sol_optimized2,leg=false)
+
+#=
+### SciPy
+model = function (t,p)
   for i in eachindex(f.params)
     setfield!(f,f.params[i],p[i])
   end
   sol = solve(prob,tspan)
   vecout = sol(t)
-  y = Matrix{Float64}(length(t),length(u0))
+  y = Matrix{Float64}(length(t),length(prob.u₀))
   for i in 1:length(t)
     y[i,:] = vecout[i]
   end
   vec(y)
 end
-
-fit = curve_fit(model,t,vec(plotseries),[1.42],show_trace=true,lambda=10000.0)
-
-#param = [3.0;5.0;5.0]
-param = fit.param
-for i in eachindex(f.params)
-  setfield!(f,f.params[i],param[i])
-end
-#prob = ODEProblem(f,u0)
-sol_optimized = solve(prob,tspan)
-plot(sol_optimized,leg=false)
-scatter!(t,plotseries)
-plot!(sol)
+using PyCall
+@pyimport scipy.optimize as opt
+opt.curve_fit(model,PyObject(t),PyObject(data))
+=#
