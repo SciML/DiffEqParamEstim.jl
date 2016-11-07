8741cc8215536a258513483578560a97fd1bf145
ChrisRackauckas <me@chrisrackauckas.com>
2016-11-07 11:54:33 -0800

    fixed for common interface

--- src/DiffEqParamEstim.jl
+++ src/DiffEqParamEstim.jl
@@ -4,39 +4,39 @@ using DiffEqBase, LsqFit, OrdinaryDiffEq, LossFunctions, RecursiveArrayTools
 
 
   ### LsqFit Method
-  function lm_fit(prob::DEProblem,tspan,t,data,p0;kwargs...)
+  function lm_fit(prob::DEProblem,t,data,p0,alg=DefaultODEAlgorithm();kwargs...)
     f = prob.f
 
     model = function (t,p)
       for i in eachindex(f.params)
         setfield!(f,f.params[i],p[i])
       end
-      sol = solve(prob,tspan)
+      sol = solve(prob,alg)
       y = vecvec_to_mat(sol(t))
       vec(y)
     end
     curve_fit(model,t,vec(data),p0;kwargs...)
   end
 
-  function build_optim_objective(prob::DEProblem,tspan,t,data;loss_func = L2DistLoss,kwargs...)
+  function build_optim_objective(prob::DEProblem,t,data,alg=DefaultODEAlgorithm();loss_func = L2DistLoss,kwargs...)
     f = prob.f
     cost_function = function (p)
       for i in eachindex(f.params)
         setfield!(f,f.params[i],p[i])
       end
-      sol = solve(prob,tspan;kwargs...)
+      sol = solve(prob,alg;kwargs...)
       y = vecvec_to_mat(sol(t))
       norm(value(loss_func(),vec(y),vec(data)))
     end
   end
 
-  function build_lsoptim_objective(prob::DEProblem,tspan,t,data;kwargs...)
+  function build_lsoptim_objective(prob::DEProblem,t,data,alg=DefaultODEAlgorithm();kwargs...)
     cost_function = function (p,out)
       f = (t,u,du) -> prob.f(t,u,du,p)
       uEltype = eltype(p)
-      u₀ = [uEltype(prob.u₀[i]) for i in 1:length(prob.u₀)]
-      temp_prob = ODEProblem(f,u₀)
-      sol = solve(temp_prob,tspan;kwargs...)
+      u0 = [uEltype(prob.u0[i]) for i in 1:length(prob.u0)]
+      temp_prob = ODEProblem(f,u0,prob.tspan)
+      sol = solve(temp_prob,alg;kwargs...)
       y = vecvec_to_mat(sol(t))
       out[:] = vec(y.-data)
     end
