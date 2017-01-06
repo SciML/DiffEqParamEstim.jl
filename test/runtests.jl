3914b4d7f6d5cf9eaaf5e327bc95da6215b3852b
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-06 01:01:06 -0800

    4 parameter

--- test/runtests.jl
+++ test/runtests.jl
@@ -74,10 +74,40 @@ result = optimize(cost_function, [1.0,2.5], BFGS())
 
 println("Use LeastSquaresOptim to fit the parameter")
 cost_function = build_lsoptim_objective(prob,t,data,Tsit5())
-x = [1.3,2.7]
+x = [1.1,2.2]
 res = optimize!(LeastSquaresProblem(x = x, f! = cost_function,
                 output_length = length(t)*length(prob.u0)),
                 LeastSquaresOptim.Dogleg(),LeastSquaresOptim.LSMR(),
                 ftol=1e-14,xtol=1e-15,iterations=100,grtol=1e-14)
 
 @test_approx_eq_eps res.minimizer [1.5;3.0] 3e-1
+
+
+f2 = @ode_def_nohes LotkaVolterraAll begin
+  dx = a*x - b*x*y
+  dy = -c*y + d*x*y
+end a=>1.5 b=>1.0 c=>3.0 d=>1.0
+
+u0 = [1.0;1.0]
+tspan = (0.0,10.0)
+prob = ODEProblem(f2,u0,tspan)
+
+println("Use LM to fit the parameter")
+fit = lm_fit(prob,t,vec(data),[1.3,0.8,2.6,1.2],Tsit5(),show_trace=true,lambda=10000.0)
+param = fit.param
+@test_approx_eq_eps param [1.5;1.0;3.0;1.0] 1e-2
+
+println("Use Optim BFGS to fit the parameter")
+cost_function = build_optim_objective(prob,t,data,Tsit5(),maxiters=10000)
+result = optimize(cost_function, [1.3,0.8,2.6,1.2], BFGS())
+@test_approx_eq_eps result.minimizer [1.5;1.0;3.0;1.0] 3e-1
+
+println("Use LeastSquaresOptim to fit the parameter")
+cost_function = build_lsoptim_objective(prob,t,data,Tsit5())
+x = [1.3,0.8,2.8,1.2]
+res = optimize!(LeastSquaresProblem(x = x, f! = cost_function,
+                output_length = length(t)*length(prob.u0)),
+                LeastSquaresOptim.Dogleg(),LeastSquaresOptim.LSMR(),
+                ftol=1e-14,xtol=1e-15,iterations=100,grtol=1e-14)
+
+@test_approx_eq_eps res.minimizer [1.5;1.0;3.0;1.0] 3e-1
