35e209eb4f9a6b5be81d71277c08d98c90abea3a
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-07 05:02:55 +0530

    cost_function3

--- src/two_stage_method.jl
+++ src/two_stage_method.jl
@@ -43,15 +43,15 @@ function two_stage_method(prob::DEProblem,tpoints,data;kwargs...)
 end
 
 
-pf_func = function (t,u,p)
-    p*u
- end
+# pf_func = function (t,u,p)
+#     p*u
+#  end
 
-pf = ParameterizedFunction(pf_func,[2])
+# pf = ParameterizedFunction(pf_func,[2])
 
-u0 = [1.0]
-tspan = (0.0,1.0)
-prob = ODEProblem(pf,u0,tspan)
+# u0 = [1.0]
+# tspan = (0.0,1.0)
+# prob = ODEProblem(pf,u0,tspan)
 
 
 # result = optimize(two_stage_method(prob,tpoints,data), 0.0, 20.0)
