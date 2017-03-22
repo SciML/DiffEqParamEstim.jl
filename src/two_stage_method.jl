da163336eedbedef953a7cc21c0ddc8b843c734d
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-23 01:15:18 +0530

    2 stage method

--- src/two_stage_method.jl
+++ src/two_stage_method.jl
@@ -1,29 +1,81 @@
 export two_stage_method
 
 # Step - 1
-function cost_function1(b::Array{Float64,2},t0,tpoints,data)
-    err = 0
-    size_two = size(data)[2]
-    for i in 1:length(tpoints)
-        w_i = exp((tpoints[i]-t0)^2)
-        for j in 1:size_two
-            temp_f = b[1,j]-(b[2,j]*(tpoints[i]-t0))
-            err += w_i*(temp_f-data[i,j])^2
-        end
+function decide_kernel(kernel)
+    if kernel == "Epanechnikov"
+        return Epanechnikov_kernel
+    elseif kernel == "Uniform"
+        return Uniform_kernel
+    else
+        return Triangular_kernel
     end
-    return err
+end
+function Epanechnikov_kernel(t)
+    if abs(t) > 1
+        return 0
+    else
+        return 0.75*(1-t^2)
+    end
+end
+
+function Uniform_kernel(t)
+    if abs(t) > 1
+        return 0
+    else
+        return 0.5
+    end
+end
+
+function Triangular_kernel(t)
+    if abs(t) > 1
+        return 0
+    else
+        return (1-abs(t))
+    end
 end
 
 
-function two_stage_method(prob::DEProblem,tpoints,data;kwargs...)
-    f = prob.f
-    b0 = typeof(data)
-    b1 = typeof(data)
+function construct_t1(t,tpoints)
+    T1 = []
     for i in 1:length(tpoints)
-        t0 = tpoints[i]
-        result = optimize(b->cost_function1(b,t0,tpoints,data), [1.0,1.0])
-        push!(b0,result.minimizer[1])
-        push!(b1,result.minimizer[2])
+        push!(T1,[1 tpoints[i]-t])
+    end
+    foldl(vcat,T1)
+    #print(T1)
+end
+function construct_t2(t,tpoints)
+    T2 = []
+    for i in 1:length(tpoints)
+        push!(T2,[1 tpoints[i]-t (tpoints[i]-t)^2])
+    end
+    foldl(vcat,T2)
+end
+function construct_w(t,tpoints,h,kernel_function)
+    n = length(tpoints)
+    W = zeros(n)
+    for i in 1:n
+        W[i] = kernel_function((tpoints[i]-t)/h)/h
+    end
+    diagm(W)
+end
+
+
+function two_stage_method(prob::DEProblem,tpoints,data,kernel="Epanechnikov";kwargs...)
+    f = prob.f
+    n = length(tpoints)
+    h = (n^(-1/5))*(n^(-3/35))*((log(n))^(-1/16))
+    estimated_solution = zeros(n,size(data)[2])
+    estimated_derivative = zeros(n,size(data)[2])
+    kernel_function = decide_kernel(kernel)
+    e1 = [1;0]
+    e2 = [0;1;0]
+
+    for i in 1:n
+        T1 = construct_t1(tpoints[i],tpoints)
+        T2 = construct_t2(tpoints[i],tpoints)
+        W = construct_w(tpoints[i],tpoints,h,kernel_function)
+        estimated_solution[i,:] = e1'*inv(T1'*W*T1)*T1'*W*data
+        estimated_derivative[i,:] = e2'*inv(T2'*W*T2)T2'*W*data
     end
 
     
@@ -32,29 +84,13 @@ function two_stage_method(prob::DEProblem,tpoints,data;kwargs...)
         ff = (t,u,du) -> prob.f(t,u,p,du)
         err = 0
         #du = zeros(length(tpoints))
-        for i in 1:length(tpoints)
-            err += (ff(tpoints[i],b0[i],b1[i]) - b1[i])^2
+        for i in 1:n
+            err += (ff(tpoints[i],estimated_solution[i],b1[i]) - estimated_derivative[i])^2
         end
         return err
     end
 end
 
-# using DifferentialEquations
-# using Optim
-# tpoints = [0.0,0.5,1.0]
-# data  = [1,exp(0.5),exp(1)]
-
-# pf_func = function (t,u,p,du)
-#     du = p*u
-#  end
-
-# pf = ParameterizedFunction(pf_func,[1])
-
-# u0 = [1.0]
-# tspan = (0.0,1.0)
-# prob = ODEProblem(pf,u0,tspan)
-# using RecursiveArrayTools
-# data = vecvec_to_mat(data)
 
 # cost_function = two_stage_method(prob,tpoints,data)
 # result = optimize(cost_function, -20.0, 20.0)
