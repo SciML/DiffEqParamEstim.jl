9c537fbba714221869f2bacc27b32f5f2ccbd438
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-06-07 00:07:41 +0530

    Resolve conflict in test_on_odes

--- test/tests_on_odes.jl
+++ test/tests_on_odes.jl
@@ -103,11 +103,6 @@ cost_function = build_loss_objective(prob,Tsit5(),CostVData(t,data),maxiters=100
 result = Optim.optimize(cost_function, [1.0,2.5], Optim.BFGS())
 @test result.minimizer ≈ [1.5;3.0] atol=3e-1
 
-println("Use Two Stage Method to fit the parameter")
-cost_function = two_stage_method(prob,t,data)
-result = Optim.optimize(cost_function, [1.0,2.5], Optim.BFGS())
-@test result.minimizer ≈ [1.5;3.0] atol=3e-1
-
 #=
 println("Use LeastSquaresOptim to fit the parameter")
 cost_function = build_lsoptim_objective(prob,t,data,Tsit5())
@@ -141,10 +136,6 @@ cost_function = build_loss_objective(prob,Tsit5(),CostVData(t,data),maxiters=100
 result = Optim.optimize(cost_function, [1.3,0.8,2.8,1.2], Optim.BFGS())
 @test result.minimizer ≈ [1.5;1.0;3.0;1.0] atol=5e-1
 
-println("Use Two Stage Method to fit the parameter")
-cost_function = two_stage_method(prob,t,data)
-result = Optim.optimize(cost_function, [1.3,0.8,2.8,1.2], Optim.BFGS())
-@test result.minimizer ≈ [1.5;1.0;3.0;1.0] atol=5e-1
 #=
 println("Use LeastSquaresOptim to fit the parameter")
 cost_function = build_lsoptim_objective(prob,t,data,Tsit5())
