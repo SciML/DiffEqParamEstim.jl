deb662146329602ec931235152a1c5f079e5bac8
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-06-15 03:59:14 +0530

    Added test

---
+++ test/tests_on_odes/build_maximum_likelihood_objective_test.jl
@@ -0,0 +1,21 @@
+using Distributions, RecursiveArrayTools
+srand(123)
+original_solution = [(sol(t[i])) for i in 1:length(t)]
+original_solution_matrix_form = vecvec_to_mat(original_solution)
+
+variance = zeros(size(original_solution_matrix_form)[1],size(original_solution_matrix_form)[2])
+error = zeros(size(original_solution_matrix_form)[1],size(original_solution_matrix_form)[2])
+
+for i in 1:size(original_solution_matrix_form)[1]
+  for j in 1:size(original_solution_matrix_form)[2]
+    tmp = rand()
+    variance[i,j] = tmp*tmp
+    d = Normal(0,tmp)
+    error[i,j] = rand(d)
+  end
+end
+#variance
+data = original_solution_matrix_form + error
+
+maximum_likelihood_function = build_maximum_likelihood_objective(prob,Tsit5(),MaximumLikelihood(t,data,variance),maxiters=10000)
+maximum_likelihood_function
