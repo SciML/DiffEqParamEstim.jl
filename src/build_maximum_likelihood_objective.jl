9bf9dade82637d7afbddc15e99c68e6c0b68d84b
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-06-15 05:59:47 +0530

    Call build_loss_objective inside

--- src/build_maximum_likelihood_objective.jl
+++ src/build_maximum_likelihood_objective.jl
@@ -9,32 +9,6 @@ end
 (f::DiffEqMaximumlikelihood)(x,y) = f.maximum_likelihood_function2(x,y)
 
 function build_maximum_likelihood_objective(prob::DEProblem,alg,loss;mpg_autodiff = false, verbose_opt = false,verbose_steps = 100, prob_generator = problem_new_parameters, kwargs...)
-  if verbose_opt
-    count = 0 # keep track of # function evaluations
-  end
-  maximum_likelihood_function = function (p)
-    if verbose_opt
-      count::Int += 1
-      if mod(count,verbose_steps) == 0
-        println("f_$count($p)")
-      end
-    end
-    tmp_prob = prob_generator(prob,p)
-    sol = solve(tmp_prob,alg;saveat=loss.t,save_everystep=false,dense=false,kwargs...)
-    loss(sol)
-  end
-
-  if mpg_autodiff
-    gcfg = ForwardDiff.GradientConfig(zeros(num_params(prob)))
-    g! = (x, out) -> ForwardDiff.gradient!(out, cost_function, x, gcfg)
-  else
-    g! = (x, out) -> Calculus.finite_difference!(cost_function,x,out,:central)
-  end
-  maximum_likelihood_function2 = function (p,grad)
-    if length(grad)>0
-      g!(p,grad)
-    end
-    maximum_likelihood_function(p)
-  end
-  DiffEqMaximumlikelihood(maximum_likelihood_function,maximum_likelihood_function2)
+  cost_function = build_loss_objective(prob::DEProblem,alg,loss;mpg_autodiff = false, verbose_opt = false,verbose_steps = 100, prob_generator = problem_new_parameters, kwargs...)
+  DiffEqMaximumlikelihood(cost_function.cost_function,cost_function.cost_function2)
 end
