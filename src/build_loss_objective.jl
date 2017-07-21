a2448ae122ea2321b3e2e3a98a1d3e122cbdc5f1
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-07-22 00:46:00 +0530

    Include if condition

--- src/build_loss_objective.jl
+++ src/build_loss_objective.jl
@@ -8,7 +8,7 @@ end
 (f::DiffEqObjective)(x) = f.cost_function(x)
 (f::DiffEqObjective)(x,y) = f.cost_function2(x,y)
 
-function build_loss_objective(prob::DEProblem,alg,loss;regularization = nothing,mpg_autodiff = false,
+function build_loss_objective(prob::DEProblem,alg,loss;regularization=nothing,mpg_autodiff = false,
                               verbose_opt = false,verbose_steps = 100,
                               prob_generator = problem_new_parameters,
                               kwargs...)
@@ -29,11 +29,11 @@ function build_loss_objective(prob::DEProblem,alg,loss;regularization = nothing,
       sol = solve(tmp_prob,alg;kwargs...)
     end
     if regularization == nothing
-      return loss(sol)
-    else 
-      return loss(sol) + regularization(p)
+      loss(sol)
+    else
+      loss(sol) + regularization(p)
     end
-end
+
   end
 
   if mpg_autodiff
