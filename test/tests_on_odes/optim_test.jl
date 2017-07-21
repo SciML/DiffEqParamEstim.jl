1b0f7b186712df02bb082cd0c414df3872673882
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-07-22 02:46:19 +0530

    Adding support for PSO and Simulated Annealing

--- test/tests_on_odes/optim_test.jl
+++ test/tests_on_odes/optim_test.jl
@@ -26,5 +26,30 @@ println("Use Optim NelderMead to fit the parameter")
 result_neldermead = Optim.optimize(cost_function2, [1.0,2.5], Optim.NelderMead())
 @test result_neldermead.minimizer ≈ [1.5;3.0] atol=3e-1
 
+println("Use Optim SimulatedAnnealing to fit the parameter")
+options = Optim.Options(iterations=100_000)
+result_simulatedannealing = Optim.optimize(obj, [1.0], Optim.SimulatedAnnealing(),options)
+@test result_simulatedannealing.minimizer ≈ [1.5] atol=3e-1
+
+result_simulatedannealing = Optim.optimize(cost_function2, [1.0,2.5], Optim.SimulatedAnnealing(),options)
+@test result_simulatedannealing.minimizer ≈ [1.5;3.0] atol=3e-1
+
+println("Use Optim Particle Swarm Optimization to fit the parameter")
+initial_x = [1.0]
+lower = [0.0]
+upper = [5.0]
+n_particles = 4
+options = Optim.Options(iterations=100)
+result_pso = Optim.optimize(obj, initial_x, Optim.ParticleSwarm(lower, upper, n_particles), options)
+@test result_pso.minimizer[1] ≈ 1.5 atol=3e-1
+
+initial_x = [1.0,2.5]
+lower = [0.0,2.0]
+upper = [5.0,10.0]
+n_particles = 4
+options = Optim.Options(iterations=100)
+result_pso = Optim.optimize(cost_function2, initial_x, Optim.ParticleSwarm(lower, upper, n_particles), options)
+@test result_pso.minimizer ≈ [1.5;3.0] atol=3e-1
+
 
 
