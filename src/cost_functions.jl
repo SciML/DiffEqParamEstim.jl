b9aeadceb14cdb2c8e0b4b1ad8eec7bc8fa29a6b
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-08-08 17:19:41 -0700

    fix stack overflow

--- src/cost_functions.jl
+++ src/cost_functions.jl
@@ -4,7 +4,7 @@ type Regularization{L,P} <: DECostFunction
   λ::L
   penalty::P
 end
-Regularization(λ,penalty=L2Penalty()) = Regularization(λ,penalty)
+Regularization(λ,penalty=L2Penalty()) = Regularization{typeof(λ),typeof(penalty)}(λ,penalty)
 
 function (f::Regularization)(p)
   f.λ*value(f.penalty, p)
