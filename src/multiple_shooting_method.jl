1ba10621f78e8265a4bdb55c884911a2deca5395
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-08-23 18:56:15 +0530

    t is extracted from the loss argument passed

--- src/multiple_shooting_method.jl
+++ src/multiple_shooting_method.jl
@@ -49,7 +49,8 @@ function multiple_shooting_method(prob::DEProblem,alg,loss,timestamp=nothing;mpg
   constraints = []
   boundary_condition = nothing
   if timestamp == nothing
-    length_of_interval = floor(length(loss.t)/10)
+    t = loss.t
+    length_of_interval = floor(length(t)/10)
     for i in 1:10
       if i==1
         construct_objective_constraints!(multiple_shooting_cost,constraints,boundary_condition,prob,prob.tspan[1],t[length_of_interval],data)
