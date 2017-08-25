19a9aa40f198affd1aef2b99ff6cb1016d974b7e
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-08-24 17:37:54 -0700

    fix test transposing

--- test/tests_on_odes/weighted_loss_test.jl
+++ test/tests_on_odes/weighted_loss_test.jl
@@ -1,8 +1,8 @@
 using Distributions, RecursiveArrayTools, NLopt
 srand(123)
 
-original_solution = [(sol(t[i])) for i in 1:length(t)]
-original_solution_matrix_form = vecvec_to_mat(original_solution)
+original_solution = VectorOfArray([(sol(t[i])) for i in 1:length(t)])
+original_solution_matrix_form = convert(Array,original_solution)
 weight = zeros(size(original_solution_matrix_form)[1],size(original_solution_matrix_form)[2])
 error = zeros(size(original_solution_matrix_form)[1],size(original_solution_matrix_form)[2])
 
@@ -17,7 +17,7 @@ end
 
 weighted_data = original_solution_matrix_form + error
 
-weighted_cost_function = build_loss_objective(prob1,Tsit5(),CostVData(t,weighted_data',weight=weight'),maxiters=10000)
+weighted_cost_function = build_loss_objective(prob1,Tsit5(),CostVData(t,weighted_data,weight=weight),maxiters=10000)
 opt = Opt(:LN_COBYLA, 1)
 min_objective!(opt, weighted_cost_function)
 (minf,minx,ret) = NLopt.optimize(opt,[1.3])
