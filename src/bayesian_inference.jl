6196ae5a8720b9765c254f8613a4ecfad0e76d4a
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-08-26 20:34:47 +0530

    Declare placeholders and use them to form the model string

--- src/bayesian_inference.jl
+++ src/bayesian_inference.jl
@@ -5,19 +5,23 @@ struct StanModel{R,C}
   chain_results::C
 end
 function bayesian_inference(prob::DEProblem,t,data;alg="integrate_ode_rk45",num_samples=1, num_warmup=1,kwargs...)
+  length_of_y = string(length(prob.u0))
+  length_of_parameter = string(length(prob.f.params))
+  
   const parameter_estimation_model = "
   functions {
     real[] sho(real t,real[] u,real[] theta,real[] x_r,int[] x_i) {
-      real du[2];  // 2 = length(prob.u0)
+      real du[$length_of_y];  // 2 = length(prob.u0) = length_of_y
+      // placeholder for differential equation here
       du[1] = theta[1] * u[1] - 1.0*u[1] * u[2]; //string(prob.f.pfuncs[1])
       du[2] = -3.0*u[1] + 1.0*u[1] * u[2];
       return du;
       }
     }
   data {
-    real u0[2]; // 2 = length(prob.u0)
+    real u0[$length_of_y]; // 2 = length(prob.u0)
     int<lower=1> T;
-    real u[T,2]; // 2 = length(prob.u0)
+    real u[T,$length_of_y]; // 2 = length(prob.u0)
     real t0;
     real ts[T];
   }
@@ -26,13 +30,13 @@ function bayesian_inference(prob::DEProblem,t,data;alg="integrate_ode_rk45",num_
     int x_i[0];
   }
   parameters {
-    vector<lower=0>[2] sigma;   // 2 = length(prob.u0)
-    real theta[1];   // // 1=length(prob.f.params)
+    vector<lower=0>[$length_of_y] sigma;   // 2 = length(prob.u0)
+    real theta[$length_of_parameter];   // // 1=length(prob.f.params)
   }
   model{
-    real u_hat[T,2]; // 2 = length(prob.u0)
+    real u_hat[T,$length_of_y]; // 2 = length(prob.u0)
     sigma ~ inv_gamma(2, 3);
-    // Define a placeholder here
+    // placeholder for priors here
     theta[1] ~ normal(1.5, 1); //1=length(prob.f.params), 1.5=prob.f.a
     u_hat = integrate_ode_rk45(sho, u0, t0, ts, theta, x_r, x_i);
     for (t in 1:T){
