97c4ba5fdbab036fa82f4e0a401d890c943c4a2e
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-08-26 19:02:55 +0530

    Added Mamba as the dependency and removed comments from model

--- src/bayesian_inference.jl
+++ src/bayesian_inference.jl
@@ -8,16 +8,16 @@ function bayesian_inference(prob::DEProblem,t,data;alg="integrate_ode_rk45",num_
   const parameter_estimation_model = "
   functions {
     real[] sho(real t,real[] y,real[] theta,real[] x_r,int[] x_i) {
-      real dydt[2];  # 2 = length(prob.u0)
+      real dydt[2];
       dydt[1] = theta[1] * y[1] - 1.0*y[1] * y[2];
       dydt[2] = -3.0*y[1] + 1.0*y[1] * y[2];
       return dydt;
       }
     }
   data {
-    real y0[2]; # 2 = length(prob.u0)
+    real y0[2];
     int<lower=1> T;
-    real y[T,2]; # 2 = length(prob.u0)
+    real y[T,2];
     real t0;
     real ts[T];
   }
@@ -26,13 +26,13 @@ function bayesian_inference(prob::DEProblem,t,data;alg="integrate_ode_rk45",num_
     int x_i[0];
   }
   parameters {
-    vector<lower=0>[2] sigma;   # 2 = length(prob.u0)
-    real theta[1];   # 1=length(prob.f.params)
+    vector<lower=0>[2] sigma;
+    real theta[1];
   }
   model{
-    real y_hat[T,2]; # 2 = length(prob.u0)
+    real y_hat[T,2];
     sigma ~ inv_gamma(2, 3);
-    theta[1] ~ normal(1.5, 1); #1=length(prob.f.params)
+    theta[1] ~ normal(1.5, 1);
     y_hat = integrate_ode_rk45(sho, y0, t0, ts, theta, x_r, x_i);
     for (t in 1:T){
       y[t] ~ normal(y_hat[t], sigma);
@@ -40,8 +40,8 @@ function bayesian_inference(prob::DEProblem,t,data;alg="integrate_ode_rk45",num_
   }
   "
 
-  stanmodel = Stan.Stanmodel(num_samples=num_samples, num_warmup=num_warmup, name="parameter_estimation_model", model=parameter_estimation_model);
+  stanmodel = Stanmodel(num_samples=num_samples, num_warmup=num_warmup, name="parameter_estimation_model", model=parameter_estimation_model);
   const parameter_estimation_data = Dict("y0"=>prob.u0, "T" => size(t)[1], "y" => data', "t0" => prob.tspan[1], "ts"=>t)
-  return_code, chain_results = Stan.stan(stanmodel, [parameter_estimation_data]; CmdStanDir=CMDSTAN_HOME)
+  return_code, chain_results = stan(stanmodel, [parameter_estimation_data]; CmdStanDir=CMDSTAN_HOME)
   return StanModel(return_code,chain_results)
 end
