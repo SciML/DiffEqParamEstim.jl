7beb4d7622bcbfde5e9c6fc2999c56be9702c24d
Unknown <vaibhavyashdixit@gmail.com>
2018-04-03 10:51:41 +0530

    Cleaner structure of code

--- src/cost_functions.jl
+++ src/cost_functions.jl
@@ -80,52 +80,50 @@ LogLikeLoss(t,data_distributions) = LogLikeLoss(t,data_distributions,nothing,not
 LogLikeLoss(t,data_distributions,diff_distributions) = LogLikeLoss(t,data_distributions,diff_distributions,1)
 
 function (f::LogLikeLoss)(sol::DESolution)
-  if f.diff_distributions == nothing
-    distributions = f.data_distributions
-    fill_length = length(f.t)-length(sol)
-    for i in 1:fill_length
-      push!(sol.u,fill(Inf,size(sol[1])))
-    end
+  distributions = f.data_distributions
+  fill_length = length(f.t)-length(sol)
+  for i in 1:fill_length
+    push!(sol.u,fill(Inf,size(sol[1])))
+  end
+  ll = 0.0
 
-    ll = 0.0
-
-    if eltype(distributions) <: UnivariateDistribution
-      for j in 1:length(f.t), i in 1:length(sol[1][1])
+  if eltype(distributions) <: UnivariateDistribution
+    for j in 1:length(f.t), i in 1:length(sol[1][1])
       # i is the number of time points
       # j is the size of the system
       # corresponds to distributions[i,j]
-        ll -= logpdf(distributions[i,j],sol[i,j])
-      end
-    else # MultivariateDistribution
-      for j in 1:length(f.t), i in 1:length(sol[1][1])
+      ll -= logpdf(distributions[i,j],sol[i,j])
+    end
+  else # MultivariateDistribution
+    for j in 1:length(f.t), i in 1:length(sol[1][1])
       # i is the number of time points
       # j is the size of the system
       # corresponds to distributions[i,j]
-        ll -= logpdf(distributions[i],sol[i])
-      end
+      ll -= logpdf(distributions[i],sol[i])
     end
-    ll
-  else
+  end
+
+  if f.diff_distributions != nothing
     distributions = f.diff_distributions
     diff_data = sol.u[2:end] - sol.u[1:end-1]
     fill_length = length(f.t)-length(diff_data)
     for i in 1:fill_length
       push!(diff_data,fill(Inf,size(sol[1])))
-    end  
-    ll = 0.0
+    end
+    fdll = 0  
     if eltype(distributions) <: UnivariateDistribution
       for j in 1:length(f.t)-1, i in 1:length(diff_data[1])
-        ll -= logpdf(distributions[j,i],diff_data[j][i])
+        fdll -= logpdf(distributions[j,i],diff_data[j][i])
       end
     else
       for j in 1:length(f.t)-1
-        ll -= logpdf(distributions[j],diff_data[j])
+        fdll -= logpdf(distributions[j],diff_data[j])
       end
     end
-    f1 = LogLikeLoss(f.t,f.data_distributions)
-    ll += f.weight*f1(sol)
-    ll
+    ll += f.weight*fdll
   end
+  
+  ll
 end
 
 function (f::LogLikeLoss)(sol::AbstractMonteCarloSolution)
