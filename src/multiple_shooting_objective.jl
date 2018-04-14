f75121a36e866f2732988526298e6c47889bb5b0
Unknown <vaibhavyashdixit@gmail.com>
2018-04-14 09:58:48 +0530

    Added generate_loss_func

--- src/multiple_shooting_objective.jl
+++ src/multiple_shooting_objective.jl
@@ -1,10 +1,20 @@
 export multiple_shooting_objective
+
+function generate_loss_func(loss,t,i)
+  new_loss = nothing
+  if typeof(loss)::L2Loss
+    new_loss = L2Loss(t,loss.data[:,i:length(t),:])
+  end
+  new_loss
+end 
+
 function multiple_shooting_objective(prob::DEProblem,alg,loss,init_N_params,regularization=nothing;mpg_autodiff = false,
                               verbose_opt = false,prob_generator = problem_new_parameters,autodiff_prototype = mpg_autodiff ? zeros(init_N_params) : nothing,
                               autodiff_chunk = mpg_autodiff ? ForwardDiff.Chunk(autodiff_prototype) : nothing,
                               kwargs...)
   cost_function = function (p)
     N = length(p)-2
+    println(length(p))
     time_len = Int(floor(length(loss.t)/N))
     time_dur = loss.t[1:time_len]
     sol = []
@@ -12,9 +22,9 @@ function multiple_shooting_objective(prob::DEProblem,alg,loss,init_N_params,regu
     for i in 1:2:N
         tmp_prob = remake(prob;u0=p[i:i+1],p=[N+1:N+2])
         if typeof(loss) <: Union{CostVData,L2Loss,LogLikeLoss}
-          # println("huihui1")
+          println("huihui1")
           push!(sol,solve(tmp_prob,alg;saveat=time_dur,save_everystep=false,dense=false,kwargs...))
-          # println("huihui2")
+          println("huihui2")
           if (i+1)*time_len < length(loss.t)
             time_dur = loss.t[i*time_len:(i+1)*time_len]
           else
@@ -23,21 +33,30 @@ function multiple_shooting_objective(prob::DEProblem,alg,loss,init_N_params,regu
         else
           push!(sol,solve(tmp_prob,alg;kwargs...))
         end
-
-        if regularization == nothing
-            loss_val += loss(sol[i])
-        else
-            loss_val += loss(sol[i]) + regularization(p[N:end])
-        end
+    end
+    time_dur = loss.t[1:time_len]
+    for i in 1:length(sol)
+      new_loss = generate_loss_func(loss,time_dur,1)
+      if (i+1)*time_len < length(loss.t)
+        time_dur = loss.t[i*time_len:(i+1)*time_len]
+      else
+        time_dur = loss.t[i*time_len:Int(length(loss.t))]
       end
-    if verbose_opt
+      if regularization == nothing
+        loss_val += new_loss(sol[i])
+      else
+        loss_val += new_loss(sol[i]) + regularization(p[N:end])
+      end
+      if verbose_opt
         count::Int += 1
         if mod(count,verbose_steps) == 0
           println("Iteration: $count")
           println("Current Cost: $loss_val")
           println("Parameters: $p")
         end
+      end
     end
+
     loss_val
   end
     if mpg_autodiff
