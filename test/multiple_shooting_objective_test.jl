e7fa805302c61c610ad97d4d8d1ea91453f18a8d
Unknown <vaibhavyashdixit@gmail.com>
2018-04-18 10:31:31 +0530

    Added weight parameter for discontinuity error

--- test/multiple_shooting_objective_test.jl
+++ test/multiple_shooting_objective_test.jl
@@ -9,9 +9,9 @@ tspan = (0.0,10.0)
 p = [1.5,1.0]
 prob1 = ODEProblem(pf_func,u0,tspan,p)
 t = collect(linspace(0,10,200))
-sol = solve(prob1,Tsit5())
-obj = multiple_shooting_objective(prob1,Tsit5(),L2Loss(t,sol),[0,0,1,1,2,2,3,3,4,4,5,5,1.2,1.2];discontinuity_weight=2.8)
-bound = Tuple{Float64, Float64}[(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10)]
+sol = solve(prob1,Tsit5(),saveat=t)
+obj = multiple_shooting_objective(prob1,Tsit5(),L2Loss(t,sol),[1,1,3,7,0,2,5,1.5,0,6,1.5,1];discontinuity_weight=10)
+bound = Tuple{Float64, Float64}[(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10),(0.5, 10)]
 result = bboptimize(obj;SearchRange = bound, MaxSteps = 11e3)
 
 @test result.archive_output.best_candidate[end-1:end] ≈ [1.5,1.0] atol = 1e-1
