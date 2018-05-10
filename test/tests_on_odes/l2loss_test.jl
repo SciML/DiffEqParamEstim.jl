e6b4c129594b1640afb6f740898c09f0d8a5e1e7
Unknown <vaibhavyashdixit@gmail.com>
2018-05-10 15:19:11 +0530

    Extend l2loss_test.jl to cover more cases

--- test/tests_on_odes/l2loss_test.jl
+++ test/tests_on_odes/l2loss_test.jl
@@ -1,4 +1,5 @@
-using BlackBoxOptim
+using BlackBoxOptim, DiffEqParamEstim, Base.Test
+include("test_problems.jl")
 
 cost_function = build_loss_objective(prob1,Tsit5(),L2Loss(t,data),
                                      maxiters=10000,verbose=false)
@@ -6,14 +7,38 @@ bound1 = Tuple{Float64, Float64}[(1, 2)]
 result = bboptimize(cost_function;SearchRange = bound1, MaxSteps = 11e3)
 @test result.archive_output.best_candidate[1] ≈ 1.5 atol=3e-1
 
-cost_function = build_loss_objective(prob2,Tsit5(),L2Loss(t,data,data_weight=0.3),
+cost_function = build_loss_objective(prob2,Tsit5(),L2Loss(t,data,data_weight=1.0),
                                      maxiters=10000,verbose=false)
 bound2 = Tuple{Float64, Float64}[(1, 2),(2, 4)]
 result = bboptimize(cost_function;SearchRange = bound2, MaxSteps = 11e3)
 @test result.archive_output.best_candidate ≈ [1.5;3.0] atol=3e-1
 
-cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data),
+cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,data_weight=nothing,differ_weight=10),
                                      maxiters=10000,verbose=false)
-bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (2, 4), (0, 2)]
+bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (1, 4), (0, 2)]
+result = bboptimize(cost_function;SearchRange = bound3, MaxSteps = 11e3)
+@test result.archive_output.best_candidate ≈ [1.5;1.0;3.0;1.0] atol=5e-1
+
+# cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,data_weight=nothing,differ_weight=10),
+#                                      maxiters=10000,verbose=false)
+# bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (2, 4), (0, 2)]
+# result = bboptimize(cost_function;SearchRange = bound3, MaxSteps = 11e3)
+# @test result.archive_output.best_candidate ≈ [1.5;1.0;3.0;1.0] atol=5e-1
+
+cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,data_weight=0.3,differ_weight=nothing),
+                                     maxiters=10000,verbose=false)
+bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (1, 4), (0, 2)]
+result = bboptimize(cost_function;SearchRange = bound3, MaxSteps = 11e3)
+@test result.archive_output.best_candidate ≈ [1.5;1.0;3.0;1.0] atol=5e-1
+
+cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,data_weight=nothing,differ_weight=nothing),
+                                     maxiters=10000,verbose=false)
+bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (1, 4), (0, 2)]
+result = bboptimize(cost_function;SearchRange = bound3, MaxSteps = 11e3)
+@test result.archive_output.best_candidate ≈ [1.5;1.0;3.0;1.0] atol=5e-1
+
+cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,data_weight=0.3,differ_weight=10),
+                                     maxiters=10000,verbose=false)
+bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (1, 4), (0, 2)]
 result = bboptimize(cost_function;SearchRange = bound3, MaxSteps = 11e3)
 @test result.archive_output.best_candidate ≈ [1.5;1.0;3.0;1.0] atol=5e-1
