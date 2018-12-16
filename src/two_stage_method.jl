766fff6765ec5190d49e393f9303b4e203aa3bbe
Unknown <vaibhavyashdixit@gmail.com>
2018-12-16 22:00:31 +0530

    Some optimisations to two_stage_method

--- src/two_stage_method.jl
+++ src/two_stage_method.jl
@@ -65,8 +65,8 @@ function construct_estimated_solution_and_derivative!(estimated_solution,estimat
       T1 = construct_t1(tpoints[i],tpoints)
       T2 = construct_t2(tpoints[i],tpoints)
       W = construct_w(tpoints[i],tpoints,h,kernel_function)
-      estimated_solution[i,:] = e1'*inv(T1'*W*T1)*T1'*W*data'
-      estimated_derivative[i,:] = e2'*inv(T2'*W*T2)T2'*W*data'
+      estimated_solution[:,i] = e1'*inv(T1'*W*T1)*T1'*W*data'
+      estimated_derivative[:,i] = e2'*inv(T2'*W*T2)T2'*W*data'
   end
 end
 
@@ -78,13 +78,12 @@ function two_stage_method(prob::DiffEqBase.DEProblem,tpoints,data;kernel= :Epane
     f = prob.f
     n = length(tpoints)
     h = (n^(-1/5))*(n^(-3/35))*((log(n))^(-1/16))
-    estimated_solution = zeros(n,size(data)[1])
-    estimated_derivative = zeros(n,size(data)[1])
+    estimated_solution = zeros(size(data)[1],n)
+    estimated_derivative = zeros(size(data)[1],n)
     kernel_function = decide_kernel(kernel)
     e1 = [1;0]
     e2 = [0;1;0]
     construct_estimated_solution_and_derivative!(estimated_solution,estimated_derivative,e1,e2,data,kernel_function,tpoints,h,n)
-
     # Step - 2
     cost_function = function (p)
         # have to adjust type for autodifferentiation
@@ -92,10 +91,11 @@ function two_stage_method(prob::DiffEqBase.DEProblem,tpoints,data;kernel= :Epane
         sol = Vector{typeof(du)}(undef,n)
         f = prob.f
         for i in 1:n
-          f(du,estimated_solution[i,:],p,tpoints[i])
+          est_sol = @view estimated_solution[:,i]
+          f(du,est_sol,p,tpoints[i])
           sol[i] = copy(du)
         end
-        sqrt(sum((vec(estimated_derivative') .- vec(VectorOfArray(sol))).^2))
+        sqrt(sum((vec(estimated_derivative) .- vec(VectorOfArray(sol))).^2))
     end
 
     if mpg_autodiff
