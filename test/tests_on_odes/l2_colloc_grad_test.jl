2646724f1d9b2ec2ff72e728b59462b2dfc266e5
yakovlev31 <v.yakovlev31@gmail.com>
2019-02-10 14:11:46 +0200

    Update l2_colloc_grad_test.jl

--- test/tests_on_odes/l2_colloc_grad_test.jl
+++ test/tests_on_odes/l2_colloc_grad_test.jl
@@ -1,20 +1,28 @@
-using BlackBoxOptim
+using DifferentialEquations, DiffEqParamEstim, Test, Optim
 
+
+weight = 1.0e-6
 
 cost_function = build_loss_objective(prob1,Tsit5(),L2Loss(t,data,colloc_grad=colloc_grad(t, data)),
                                      maxiters=10000,verbose=false)
-bound1 = Tuple{Float64, Float64}[(1, 2)]
-result = bboptimize(cost_function;SearchRange = bound1, MaxSteps = 11e3)
-@test result.archive_output.best_candidate[1] ≈ 1.5 atol=3e-1
+result = Optim.optimize(cost_function, 1., 2)
+@test result.minimizer ≈ 1.5 atol=3e-1
 
-cost_function = build_loss_objective(prob2,Tsit5(),L2Loss(t,data,colloc_grad=colloc_grad(t, data)),
-                                     maxiters=10000,verbose=false)
-bound2 = Tuple{Float64, Float64}[(1, 2),(1, 4)]
-result = bboptimize(cost_function;SearchRange = bound2, MaxSteps = 11e3)
-@test result.archive_output.best_candidate ≈ [1.5;3.0] atol=3e-1
 
-cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,colloc_grad=colloc_grad(t, data)),
+cost_function = build_loss_objective(prob2,Tsit5(),L2Loss(t,data,
+  differ_weight=weight,data_weight=weight,colloc_grad=colloc_grad(t, data)),
                                      maxiters=10000,verbose=false)
-bound3 = Tuple{Float64, Float64}[(1, 2),(0, 2), (2, 4), (0, 2)]
-result = bboptimize(cost_function;SearchRange = bound3, MaxSteps = 11e3)
-@test result.archive_output.best_candidate ≈ [1.5;1.0;3.0;1.0] atol=5e-1
+result = Optim.optimize(cost_function, [1.3,2.8], Optim.BFGS())
+@test result.minimizer ≈ [1.5;3.0] atol=3e-1
+
+
+cost_function = build_loss_objective(prob3,Tsit5(),L2Loss(t,data,
+  differ_weight=weight,colloc_grad=colloc_grad(t, data)), maxiters=10000,verbose=false)
+result = Optim.optimize(cost_function, [1.3,0.8,2.8,1.2], Optim.BFGS())
+@test result.minimizer ≈ [1.5,1.0,3.0,1.0] atol=3e-1
+
+
+cost_function = build_loss_objective(prob1,Tsit5(),L2Loss(t,data,
+  data_weight=weight,colloc_grad=colloc_grad(t, data)),maxiters=10000,verbose=false)
+result = Optim.optimize(cost_function, 1., 2)
+@test result.minimizer ≈ 1.5 atol=3e-1
