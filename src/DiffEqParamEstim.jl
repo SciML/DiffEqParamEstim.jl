7bf8f89dc4e7f4e7367b12c40a4d0f1fcb42c269
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-02-25 08:34:12 -0500

    fix prob generator to pass along monte carlo args

--- src/DiffEqParamEstim.jl
+++ src/DiffEqParamEstim.jl
@@ -3,8 +3,12 @@ using DiffEqBase, LsqFit, PenaltyFunctions,
       RecursiveArrayTools, ForwardDiff, Calculus, Distributions, LinearAlgebra, DiffEqSensitivity, Dierckx
 
 STANDARD_PROB_GENERATOR(prob,p) = remake(prob;u0=eltype(p).(prob.u0),p=p)
-STANDARD_PROB_GENERATOR(prob::MonteCarloProblem,p) = MonteCarloProblem(remake(prob.prob;u0=eltype(p).(prob.prob.u0),p=p))
-
+STANDARD_PROB_GENERATOR(prob::MonteCarloProblem,p) = MonteCarloProblem(
+                             remake(prob.prob;u0=eltype(p).(prob.prob.u0),p=p),
+                             output_func = prob.output_func,
+                             prob_func = prob.prob_func,
+                             reduction = prob.reduction,
+                             u_init = prob.u_init)
 include("cost_functions.jl")
 include("lm_fit.jl")
 include("build_loss_objective.jl")
