7eee5ad4f881c0ac62af9609b3ed6cbb29b75175
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-11-22 16:51:13 -0500

    Update regularization_test.jl

--- test/tests_on_odes/regularization_test.jl
+++ test/tests_on_odes/regularization_test.jl
@@ -8,11 +8,11 @@ cost_function_3 = build_loss_objective(prob3,Tsit5(),L2Loss(t,data),verbose=fals
                    Regularization(0.1,MahalanobisPenalty(Matrix(1.0I, 4, 4))),maxiters=10000)
 
 println("Use Optim BFGS to fit the parameter")
-result = Optim.optimize(cost_function_1, [1.0], Optim.BFGS())
+result = Optim.optimize(cost_function_1, [1.0], Optim.BFGS(),abstol=1e-8,reltol=1e-8)
 @test result.minimizer[1] ≈ 1.5 atol=3e-1
 
-result_bfgs = Optim.optimize(cost_function_2, [1.2,2.7], Optim.BFGS())
+result_bfgs = Optim.optimize(cost_function_2, [1.2,2.7], Optim.BFGS(),abstol=1e-8,reltol=1e-8)
 @test result_bfgs.minimizer ≈ [1.5;3.0] atol=3e-1
 
-result_bfgs = Optim.optimize(cost_function_3, [1.3,0.8,2.8,1.2], Optim.BFGS())
+result_bfgs = Optim.optimize(cost_function_3, [1.3,0.8,2.8,1.2], Optim.BFGS(),abstol=1e-8,reltol=1e-8)
 @test result_bfgs.minimizer ≈ [1.5;1.0;3.0;1.0] atol=5e-1
