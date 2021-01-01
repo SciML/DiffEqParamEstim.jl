9b3be1eefcc47c78c0c30b6e4df56960b8b1fcc4
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2021-01-01 12:10:46 +0530

    Looser variance in multiple shooting test priors

--- test/multiple_shooting_objective_test.jl
+++ test/multiple_shooting_objective_test.jl
@@ -19,7 +19,7 @@ ms_obj = multiple_shooting_objective(ms_prob,Tsit5(),L2Loss(t,data);discontinuit
 result = bboptimize(ms_obj;SearchRange = bound, MaxSteps = 21e3)
 @test result.archive_output.best_candidate[end-1:end] ≈ [1.5,1.0] atol = 2e-1
 
-priors = [Truncated(Normal(1.5,0.1),0,2),Truncated(Normal(1.0,0.1),0,1.5)]
+priors = [Truncated(Normal(1.5,0.5),0,2),Truncated(Normal(1.0,0.5),0,1.5)]
 ms_obj1 = multiple_shooting_objective(ms_prob,Tsit5(),L2Loss(t,data);priors=priors,discontinuity_weight=1.0,abstol=1e-12,reltol=1e-12)
 result = bboptimize(ms_obj1;SearchRange = bound, MaxSteps = 21e3)
 @test result.archive_output.best_candidate[end-1:end] ≈ [1.5,1.0] atol = 2e-1
