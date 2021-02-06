b5a022c99efafe8a027c8d1e8564a46c34424ac7
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2021-02-05 23:32:31 -0500

    Update cost_functions.jl

--- src/cost_functions.jl
+++ src/cost_functions.jl
@@ -108,7 +108,7 @@ function (f::L2Loss)(sol::DiffEqBase.DESolution)
           sumsq = sumsq + ((data[j,i] - sol[j,i])^2)*weight[j,i]
         end
       end
-      if diff_weight != nothing
+      if diff_weight != nothing && i != 1
         for j in 1:length(sol[i])
           if typeof(diff_weight) <: Real
             sumsq += diff_weight*((data[j,i] - data[j,i-1] - sol[j,i] + sol[j,i-1])^2)
