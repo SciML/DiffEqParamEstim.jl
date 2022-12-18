dc146246641943a585a72183a1d9fad78b538673
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-12-18 07:13:32 -0500

    Update src/two_stage_method.jl

--- src/two_stage_method.jl
+++ src/two_stage_method.jl
@@ -86,7 +86,7 @@ function construct_iip_cost_function(f, du, preview_est_sol, preview_est_deriv, 
 end
 
 function construct_oop_cost_function(f, du, preview_est_sol, preview_est_deriv, tpoints)
-    function (p, nothing)
+    function (p, _)
         cost = zero(first(p))
         for i in 1:length(preview_est_sol)
             est_sol = preview_est_sol[i]
