89d062e0bd1058816cdba63fdb6afbe5e06a90e3
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2024-02-18 03:20:50 -0500

    Update regularization_test.jl

--- test/tests_on_odes/regularization_test.jl
+++ test/tests_on_odes/regularization_test.jl
@@ -21,13 +21,13 @@ cost_function_3 = build_loss_objective(prob3, Tsit5(), L2Loss(t, data),
 
 println("Use Optim BFGS to fit the parameter")
 optprob = Optimization.OptimizationProblem(cost_function_1, [1.0])
-result = solve(optprob, Optim.LBFGS(), maxiters = 500)
+result = solve(optprob, Optim.LBFGS())
 @test result.u[1]≈1.5 atol=3e-1
 
 optprob = Optimization.OptimizationProblem(cost_function_2, [1.2, 2.7])
-result = solve(optprob, Optim.LBFGS(), maxiters = 500)
+result = solve(optprob, Optim.LBFGS())
 @test result.minimizer≈[1.5; 3.0] atol=3e-1
 
 optprob = Optimization.OptimizationProblem(cost_function_3, [1.3, 0.8, 2.8, 1.2])
-result = solve(optprob, Optim.LBFGS(), maxiters = 500)
+result = solve(optprob, Optim.LBFGS())
 @test result.minimizer≈[1.5; 1.0; 3.0; 1.0] atol=5e-1
