6f1b0deacd73ffff24caefc383611324f2eadac8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-07-31 07:09:17 -0400

    Update regularization_test.jl

--- test/tests_on_odes/regularization_test.jl
+++ test/tests_on_odes/regularization_test.jl
@@ -26,8 +26,8 @@ result = solve(optprob, Optim.BFGS())
 
 optprob = Optimization.OptimizationProblem(cost_function_2, [1.2, 2.7])
 result = solve(optprob, Optim.BFGS())
-@test result.minimizer≈[1.5; 3.0] atol=3e-1
+@test result.u≈[1.5; 3.0] atol=3e-1
 
 optprob = Optimization.OptimizationProblem(cost_function_3, [1.3, 0.8, 2.8, 1.2])
 result = solve(optprob, Optim.BFGS())
-@test result.minimizer≈[1.5; 1.0; 3.0; 1.0] atol=5e-1
+@test result.u≈[1.5; 1.0; 3.0; 1.0] atol=5e-1
