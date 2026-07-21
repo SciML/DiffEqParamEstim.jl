f078a9803a8252bfdd0b642d295660a3b5834193
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-21 02:09:59 -0400

    Use SciMLTesting 2.4 QA defaults
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -2,8 +2,6 @@ using SciMLTesting, DiffEqParamEstim, JET, Test
 
 run_qa(
     DiffEqParamEstim;
-    api_docs_kwargs = (; rendered = true),
-    explicit_imports = true,
     ei_kwargs = (
         # NoAD is owned by SciMLBase but not (yet) declared public there.
         all_qualified_accesses_are_public = (;
