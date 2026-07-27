6d3fffdb9e912bebe7e98cdf1484eed03e47d9fb
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-26 20:11:37 -0400

    Require strict SciMLTesting 2.4 QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,13 +1,3 @@
-using SciMLTesting, DiffEqParamEstim, JET, Test
+using SciMLTesting, DiffEqParamEstim
 
-run_qa(
-    DiffEqParamEstim;
-    ei_kwargs = (
-        # NoAD is owned by SciMLBase but not (yet) declared public there.
-        all_qualified_accesses_are_public = (;
-            ignore = (
-                :NoAD,  # SciMLBase
-            ),
-        ),
-    ),
-)
+run_qa(DiffEqParamEstim)
